928b3889fd3245983ff60526651f52a22bbe7bde
Fábio <fabio.so2307@hotmail.com>
2026-02-26 02:48:06 -0300

    ajustes

--- assets/js/blog-slider.js
+++ assets/js/blog-slider.js
@@ -1,28 +1,18 @@
 /*--------------- Blog Slider ---------------*/
 blogSwiper = new Swiper(".blog-slider", {
     spaceBetween: 20,
-    speed: 600, // movimento mais suave
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
+    loop: false, // 🔥 desativa loop definitivamente
+    autoplay: false, // 🔥 remove autoplay (evita reposicionamento automático)
     pagination: {
         el: ".swiper-pagination2",
         clickable: true,
     },
-
+    watchOverflow: true,
+    observer: true,
+    observeParents: true,
     breakpoints: {
-        0: {
-            slidesPerView: 1,
-            loop: false, // 🔥 desativa loop no mobile
-        },
-        768: {
-            slidesPerView: 2,
-            loop: true,
-        },
-        1024: {
-            slidesPerView: 3,
-            loop: true,
-        },
+        0: { slidesPerView: 1 },
+        768: { slidesPerView: 2 },
+        1024: { slidesPerView: 3 },
     },
 });
