a3d0b5826ce79fc125b3de0b13cf1413eeb03224
Fábio <fabio.so2307@hotmail.com>
2026-08-04 02:50:15 -0300

    corrigindo versão

--- netlify/functions/calendar.js
+++ netlify/functions/calendar.js
@@ -2,7 +2,7 @@ exports.handler = async function () {
 
     const apiKey = process.env.API_KEY;
 
-    const calendarId = process.env.CAlEN_ID;
+    const calendarId = "c56f0345c8a935d9d4c595146ab880a8a72983c0196a5b35428c292f3e74eb9b@group.calendar.google.com";
 
     const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}`;
 
